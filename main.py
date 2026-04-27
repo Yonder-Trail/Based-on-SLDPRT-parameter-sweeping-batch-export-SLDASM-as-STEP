613758eafa432c3259a7727a0086133a797e80d0
Yonder•Trail <yutianlebuaa@gmail.com>
2026-04-27 21:59:00 +0800

    Update main.py

--- main.py
+++ main.py
@@ -19,28 +19,29 @@ def SLDASM_to_STEP_NewRebuild(Global_Parameter,Global_Parameter_list,SLDPRT_File
     def Create_Directory(Dir_path):
         if not os.path.exists(Dir_path):
             os.makedirs(Dir_path)
+    
     #获取当前进程信息
-    def Get_Progcess_Information(start_time,Countindex,SUM,Planindex):
+    def Get_Progcess_Information(start_time,Countindex,SUMs,Planindex):
         global SW_app
         Elapsed_Time=time.time()-start_time
         Speed=Elapsed_Time/Countindex
-        Remaining_Sec=(SUM-Countindex)*Speed
+        Remaining_Sec=(SUMs-Countindex)*Speed
         Remaining_Time=timedelta(seconds=int(Remaining_Sec))
         Eta_Time=datetime.now()+timedelta(seconds=Remaining_Sec)
         Eta_str=Eta_Time.strftime("%Y-%m-%d %H:%M:%S")
         Elapsed_str=str(timedelta(seconds=int(Elapsed_Time)))
-        Progress=(Countindex/SUM)*100
-        if Countindex%200==0:
+        Progress=(Countindex/SUMs)*100
+        print(f"[OK] 当前计划 {Planindex} | 完成 {(Countindex)}/{SUMs} | 进度：{Progress:.3f}%    [Time] 已用时间：{Elapsed_str} | 剩余时间：{Remaining_Time} | 预计完成：{Eta_str}")
+        if Countindex%50==0:
             #重新启动SolidWorks软件，释放内存空间
             SW_app.ExitApp()
             del SW_app
             gc.collect()
             subprocess.call('taskkill /f /im SLDWORKS.exe', shell=True)
             SW_app=win32com.client.Dispatch("SldWorks.Application")
-            SW_app.Visible=False
-        print(f"[OK] 当前计划 {Planindex} | 完成 {(Countindex)}/{SUM} | 进度：{Progress:.3f}%    [Time] 已用时间：{Elapsed_str} | 剩余时间：{Remaining_Time} | 预计完成：{Eta_str}")
-
-    #重建装配体
+            SW_app.Visible=False#可视化
+    
+    #创建基于全局变量循环的重建装配体函数
     def Rebuild_SLDASM_to_STEP(Global_Parameter,Global_Parameter_list,SLDPRT_Dir,SLDASM_Target_Files,Output_STEP_Incomplete_Dir,Planindex):
         global SW_app
         #全局变量的各参量组合
@@ -50,12 +51,12 @@ def SLDASM_to_STEP_NewRebuild(Global_Parameter,Global_Parameter_list,SLDPRT_File
             All_Parameter_Combinations.append(np.arange(Lower_Limit_Value,Upper_Limit_Value,Step_Value))
         # 启动SolidWorks软件
         SW_app=win32com.client.Dispatch("SldWorks.Application")
-        SW_app.Visible=False
+        SW_app.Visible=False#可视化
         #定义错误与警告变量   
         Errors=VARIANT(pythoncom.VT_BYREF|pythoncom.VT_I4,0)
         Warnings=VARIANT(pythoncom.VT_BYREF|pythoncom.VT_I4,0)
         #总数
-        SUM=math.prod([len(Parameter_Arr) for Parameter_Arr in All_Parameter_Combinations])*len(SLDASM_Target_Files)
+        SUMs=math.prod([len(Parameter_Arr) for Parameter_Arr in All_Parameter_Combinations])*len(SLDASM_Target_Files)
         start_time=time.time()#开始时间
         Countindex=1#当前进度
         for _,Parameter_Combination in enumerate(itertools.product(*All_Parameter_Combinations)):
@@ -83,7 +84,7 @@ def SLDASM_to_STEP_NewRebuild(Global_Parameter,Global_Parameter_list,SLDPRT_File
             gc.collect()#回收内存
             #导出为STEP文件
             for SLDASM_Target_File_Index,SLDASM_Target_File in enumerate(SLDASM_Target_Files):
-                Output_STEP_Complete_Dir=Output_STEP_Incomplete_Dir[SLDASM_Target_File_Index]+f"_{Countindex}.step"
+                Output_STEP_Complete_Dir=Output_STEP_Incomplete_Dir[SLDASM_Target_File_Index]+f"{Countindex}.step"
                 SW_SLDASM_Doc=SW_app.OpenDoc6(SLDASM_Target_File,2,1,"",Errors,Warnings)#打开装配体(数字2表示装配体文件)
                 SW_models=SW_app.ActiveDoc
                 SW_models.ForceRebuild3(True)#强制重建
@@ -91,21 +92,22 @@ def SLDASM_to_STEP_NewRebuild(Global_Parameter,Global_Parameter_list,SLDPRT_File
                 SW_app.CloseDoc(f"{os.path.splitext(os.path.basename(SLDASM_Target_File))[0]}.SLDASM")
                 del SW_models,SW_SLDASM_Doc
                 gc.collect()#回收内存
-                Get_Progcess_Information(start_time,Countindex,SUM,Planindex)
+                Get_Progcess_Information(start_time,Countindex,SUMs,Planindex)
                 Countindex=Countindex+1
-        SW_app.ExitApp()               
+        SW_app.ExitApp()   
+                
     #创建输出目录
     Create_Directory(Output_STEP_Dir) 
     #目标装配体路径地址完整声明
     SLDASM_Target_Files=[]
     for SLDPRT_Filesname_target in SLDPRT_Filesname:
         SLDASM_Target_Files.append(os.path.join(SLDASM_files_Dir,f"{SLDPRT_Filesname_target}.SLDASM").replace('\\', '/'))
-    #装配体STEP输出路径地址非完整声明，后续需进行完整声明，在Rebuild_SLDASM_to_STEP()函数中见Output_STEP_Complete_Dir变量
+    #装配体STEP输出路径地址非完整声明，后续需进行完整声明
     Output_STEP_Incomplete_Dir=[]
     for SLDPRT_Filesname_target in SLDASM_Target_Files:
         File_name=os.path.splitext(os.path.basename(SLDPRT_Filesname_target))[0]
         Output_STEP_Incomplete_Dir.append(os.path.join(Output_STEP_Dir, f"{File_name}_{Componentindex}_").replace('\\', '/'))
-    #重建装配体并转STEP
+    #重建装配体STEP
     Rebuild_SLDASM_to_STEP(Global_Parameter,Global_Parameter_list,SLDPRT_Dir,SLDASM_Target_Files,Output_STEP_Incomplete_Dir,Planindex)
     
 if __name__ == "__main__":
